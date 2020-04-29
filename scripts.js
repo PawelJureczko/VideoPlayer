d29dcc5ad2780d27b57b4888abdc6635d8220888
Paweu <paloboo@o2.pl>
2020-04-29 14:28:57 +0200

    toggle buttons programed

--- scripts.js
+++ scripts.js
@@ -5,3 +5,26 @@ const progressBar = player.querySelector(".progress_filled");
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+
+
+function togglePlay(){
+    if(video.paused){
+        video.play();
+        toggle.textContent = "ll";
+    } else {
+        video.pause();
+        toggle.textContent = "►";
+    }
+}
+
+function skip (){
+    video.currentTime+=parseInt(this.dataset.skip);
+}
+
+
+skipButtons.forEach(item=>{
+    item.addEventListener("click", skip);
+})
+toggle.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
